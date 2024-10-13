f962fee5c1006f728816be299dc315edf06f4c5e
LinMeng729 <102458185+LinMeng729@users.noreply.github.com>
2024-10-13 21:14:15 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function processFormData() {
     // var lel = document.getElementById('lel');
     // var tex = document.getElementById('tex');
@@ -8,13 +9,16 @@ function processFormData() {
     var fast = parseInt(document.getElementById('fast').value)
     var energy = parseInt(document.getElementById('energy').value)
     var n =(lel-energy-tex)/fast/60;
+    n=Math.round( n ) ;
     alert('滿經驗需要 ' + n + ' 分鐘' );
 }
 function processFormData2() {
     var power = parseInt(document.getElementById('power').value)
     var x =power*1.05/0.95;
+    x=Math.round( x ) ;
     var y =power*0.95/1.05;
-    alert('可以打到 ' + x + '/n戰力滿'+ y + ' 可打');
+    y=Math.round( y ) ;
+    alert('可打到 ' + x + '\n戰力滿'+ y + ' 可打我');
 }
 function processFormData3() {
     var roomnum = parseInt(document.getElementById('roomnum').value)
@@ -22,8 +26,9 @@ function processFormData3() {
     if(roomnum%2!=0){
         roomnum=(roomnum+1)/2;
     }
-    roomnum=roomnum/2;
-    i++;
+    else{
+        roomnum=roomnum/2;
+    }
     }
     alert('共有 ' + roomnum + ' 間靈泉房' );
 }
