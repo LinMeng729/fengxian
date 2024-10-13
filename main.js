dd2cad98d82323727a0da7a4ae91762668cc7ae6
LinMeng729 <102458185+LinMeng729@users.noreply.github.com>
2024-10-13 20:14:04 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -10,3 +10,20 @@ function processFormData() {
     var n =(lel-energy-tex)/fast/60;
     alert('滿經驗需要 ' + n + ' 分鐘' );
 }
+function processFormData2() {
+    var power = parseInt(document.getElementById('power').value)
+    var x =power*1.05/0.95;
+    var y =power*0.95/1.05;
+    alert('可以打到 ' + x + '/n戰力滿'+ y + ' 可打');
+}
+function processFormData3() {
+    var roomnum = parseInt(document.getElementById('roomnum').value)
+    for(var i=1;i<=5;i++){
+    if(roomnum%2!=0){
+        roomnum=(roomnum+1)/2;
+    }
+    roomnum=roomnum/2;
+    i++;
+    }
+    alert('共有 ' + roomnum + ' 間靈泉房' );
+}
